a392ca4c5aa5dc36c44b512d952873dcfba2609b
chris-mayamba <christianmayamba04@gmail.com>
2025-02-01 23:33:43 +0200

    Ajout de la description et de la liste interactive

--- app/src/main/java/com/example/drapeau/MainActivity.kt
+++ app/src/main/java/com/example/drapeau/MainActivity.kt
@@ -6,51 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.calculateEndPadding
-import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 //import com.example.drapeau.data.Datasource
 import com.example.drapeau.data.Flag
 import com.example.drapeau.data.flags
-import com.example.drapeau.model.Drapeau
 import com.example.drapeau.ui.theme.DrapeauTheme
 
 class MainActivity : ComponentActivity() {
@@ -90,16 +82,50 @@ fun FlagItem(
     flag : Flag,
     modifier: Modifier = Modifier
     ){
-    Card(modifier = Modifier.padding(8.dp)) {
+
+    val expanded = remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable { expanded.value = !expanded.value }
+    ) {
         Row(
             modifier = modifier
                 .fillMaxWidth()
-                .padding(dimensionResource(id = R.dimen.padding_small))
+                .padding(dimensionResource(id = R.dimen.padding_small)),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
 
-        ){
-            FlagIcon(flag.imageResourceId)
-            FlagInformation(flag.name, flag.capital, flag.code)
+        ) {
+            Row(
+                modifier = Modifier.weight(1f),
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                FlagIcon(flag.imageResourceId)
+                FlagInformation(
+                    flag.name,
+                    flag.capital,
+                    flag.code,
+                    flag.desc,
+                    expanded = expanded.value
+                )
+            }
+            Icon(
+                imageVector = if (expanded.value)
+                    Icons.Filled.KeyboardArrowUp
+                else
+                    Icons.Filled.KeyboardArrowDown,
+
+                contentDescription = if (expanded.value) "Réduire"
+                else "Afficher",
+                modifier = Modifier.padding(start = 8.dp)
+            )
+
         }
+
+
     }
 
 }
@@ -124,11 +150,17 @@ fun FlagIcon(
 
 @Composable
 fun FlagInformation(
-    @StringRes flagName : Int,
-    @StringRes flagcode : Int,
-    @StringRes flagcapital : Int,
-    modifier: Modifier = Modifier
+    @StringRes flagName: Int,
+    @StringRes flagcode: Int,
+    @StringRes flagcapital: Int,
+    @StringRes flagdesc: Int,
+
+    expanded:Boolean,
+
+    modifier: Modifier = Modifier,
+
 ){
+
     Column(modifier = modifier){
         Text(
             text = stringResource(flagName),
@@ -144,6 +176,13 @@ fun FlagInformation(
             Text(
                 text = stringResource(flagcode),
                 style = MaterialTheme.typography.bodyLarge
+            )
+
+        }
+        AnimatedVisibility(visible = expanded) {
+            Text(
+                text = stringResource(flagdesc),
+                modifier = Modifier.padding(top = 8.dp)
             )
         }
 
